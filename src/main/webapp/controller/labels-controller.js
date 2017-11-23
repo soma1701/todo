27a19d8dc543982b3e2a0dffee9a8af0e37254b5
soma1701 <you@example.com>
2017-11-24 01:03:01 +0530

    rest functionality is not working stuck

--- src/main/webapp/controller/labels-controller.js
+++ src/main/webapp/controller/labels-controller.js
@@ -1,14 +1,16 @@
 toDoApp.controller('labelsController',function($scope, $location,labelService){
 	var path = $location.path();
 	var labelName = path.substr(path.lastIndexOf("/")+1);
-	var httpGetNotes = labelService.getLabelNotes(labelName);
 	
-	httpGetNotes.then(function(response) {
-		$scope.notes = response.data;
-	}, function(response) {
-		if(response.status=='400')
-			$location.path('/loginPage')
-			console.log(response);
-	});
+	function getLabels(){
+		var getLabels = labelService.getLabelNotes(labelName);
+		httpGetNotes.then(function(response) {
+			$scope.notes = response.data;
+		}, function(response) {
+			if(response.status=='400')
+				$location.path('/loginPage')
+				console.log(response);
+		});
+	}
 });
 
